aa938f7baf5802b2e3662026c0d97c322e789a9c
Greg Parr <greg.parr@blueyonder.co.uk>
2018-02-17 23:22:53 +0000

    Some refactoring, added innings for second team, added match result calculation

--- game.py
+++ game.py
@@ -2,15 +2,18 @@ import random
 import time
 
 allOutWickets = 10 # can reduce this down for testing
-gameRate = 1 # seconds between balls
+gameRate = 0.25 # seconds between balls
 balls = ["0", "1", "2", "3", "4", "6", "Owzthat!"]
 decisions = ["Not Out!","Bowled!","Caught!","LBW!","Run Out!","Stumped!","Not Out!"]
+noScoreToBeat = -1
+team1 = "Sussex"
+team2 = "Kent"
 
 def returnToMark():
     time.sleep(gameRate)
 
 def makeDecision():
-    time.sleep(gameRate * 5)
+    time.sleep(gameRate * 2)
 
 def roll(dice):
     if (dice == "bowl"):
@@ -21,25 +24,45 @@ def roll(dice):
     else:
         return "0"
 
-def innings():
+def innings(scoreToBeat):
     wickets = 0
     score = 0
+    bowled = 0
 
-    while wickets < allOutWickets:
+    while wickets < allOutWickets and (not score > scoreToBeat or scoreToBeat == noScoreToBeat):
         ball = roll("bowl")
-        print(ball)
+        event = ""
         if (ball == "Owzthat!"):
+            print(ball)
             decision = roll("decision")
-            print(decision)
+            event = decision
             if (decision != "Not Out!"):
                 wickets = wickets + 1
         else:
+            event = "{} run".format(ball)
+            if (ball != "1"):
+                event = event + "s"
+
             score = score + int(ball)
             
-        print("{}/{}".format(score,wickets))
-        
+        bowled = bowled + 1
+        balls = int(bowled % 6)
+        overs = int((bowled - balls) / 6)
+        print("{} - {}/{} ({}.{} overs)".format(event,score,wickets,overs,balls))
+
         returnToMark()
 
-    print("Innings over!")
 
-innings()
+    return { "Runs": score, "Wickets": wickets, "Overs": overs, "Balls": balls }
+
+firstInnings = innings(noScoreToBeat)
+print("{} scores {}/{} in {}.{} overs".format(team1,firstInnings["Runs"],firstInnings["Wickets"],firstInnings["Overs"],firstInnings["Balls"]))
+secondInnings = innings(firstInnings["Runs"])
+print("{} scores {}/{} in {}.{} overs".format(team2,secondInnings["Runs"],secondInnings["Wickets"],secondInnings["Overs"],secondInnings["Balls"]))
+
+if (firstInnings["Runs"] > secondInnings["Runs"]):
+    print("{} win by {} runs!".format(team1,firstInnings["Runs"] - secondInnings["Runs"]))
+elif (firstInnings["Runs"] < secondInnings["Runs"]):
+    print("{} win by {} wickets!".format(team2,10 - secondInnings["Wickets"]))
+else:
+    print("Tied game!")
